a55b38d4eb3b009a041a438d58005a34b892e8ac
Satyam Gehani <Satyam.Gehani@nology.io>
2019-01-03 15:38:42 +0000

    register input fields, auth service created

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
+import { AuthService } from "../services/auth.service";
 
 @Component({
   selector: 'app-register',
@@ -6,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  name = '';
+  email = '';
+  password = '';
+  confirmPassword = '';
+  errorMessage: string;
 
-  constructor() { }
+  constructor(private router: Router, private auth: AuthService) { }
+
+  register(details: {email: string, password: string}) {
+
+    this.auth.register(details.email, details.password)
+    .then(()=>{
+      this.router.navigate(['/']);
+      console.log("Great success")
+    })
+    .catch((error: Error)=>{
+      this.errorMessage = error.message;
+    });
+  }
 
   ngOnInit() {
   }
-
 }
